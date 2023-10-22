a3752eca99fb8eef3ffe0c068cfbe9613d98a1cc
bonnsuke <onewild79@gmail.com>
2023-10-22 14:34:44 +0900

    composer同期

---
+++ app/Models/Item.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+    protected $table = 'items';
+    protected $fillable = ['user_id', 'name', 'type_id', 'detail'];
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+        // この商品は一つの種別に属している (逆の関係もTypeモデル内で定義する)
+    }
+    
+}
