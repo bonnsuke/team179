50f9f4f88e896a590b08f0bd3cbccf2db3b2ead6
bonnsuke <onewild79@gmail.com>
2023-10-26 08:55:37 +0900

    詳細の行をクリックすると全表示

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app');
+@extends('layouts.app')
 
 @section('content')
     <!-- フラッシュメッセージ -->
@@ -7,56 +7,97 @@
             {{ session('flash_message') }}
         </div>
     @endif
+
     <form action="/search" method="get" class="d-inline-block">
-      @csrf
-      <input type="text" name="keyword">
-      <input type="submit" value="検索">
-  </form>
-  @if(isset($count) && $count > 0)
-      <p>検索結果: {{ $count }} 件</p>
-  @endif
+        @csrf
+        <input type="text" name="keyword">
+        <input type="submit" value="検索">
+    </form>
+
+    @if(isset($count) && $count > 0)
+        <p>検索結果: {{ $count }} 件</p>
+    @endif
+
     @can('admin-role')
-      <a href="/create" class="btn btn-success d-inline-block mx-3">商品登録</a>
+        <a href="/create" class="btn btn-success d-inline-block mx-3">商品登録</a>
     @endcan
+
     <div>
-    <a href="/main_home">ホーム画面へ</a>
+        <a href="/main_home">ホーム画面へ</a>
     </div>
+
     {{-- ログアウト --}}
     <form action="{{ route('logout') }}" method="POST">
-      @csrf
-      <button type="submit">ログアウト</button>
-  </form>
-    </div>
+        @csrf
+        <button type="submit">ログアウト</button>
+    </form>
+
     <table class="table table-striped table-responsive">
-      <thead>
-        <tr>
-          <th style = "width: 10%">ID</th>
-          <th style = "width: 25%">名前</th>
-          <th style = "width: 25%">種別</th>
-          <th style = "width: 30%">詳細</th>
-          @can('admin-role')
-          <th style = "width: 10%">編集</th>
-          @endcan
-        </tr>
-      </thead>
-    <tbody>
-    @foreach ($items as $item)
-      <tr>
-        <td>{{ $item->id }} </td>
-        <td>{{ $item->name }}</td>
-        <td>{{ $item->type_name }} </td>
-        <td>{{ $item->detail }}</td>
-        @can('admin-role')
-        <td>
-          <a href="{{ route('items.edit', $item->id) }}" class="btn btn-primary">編集</a>
-          <form action="/destroy/{{$item->id}}" method="post">
-            @csrf
-            <button class=" btn btn-danger" type="submit">削除</button>
-          </form>
-        </td>
-        @endcan
-      </tr>
-      @endforeach
-    </tbody>
-  </table>
+        <thead>
+            <tr>
+                <th style="width: 10%">ID</th>
+                <th style="width: 25%">名前</th>
+                <th style="width: 25%">種別</th>
+                <th style="width: 30%">詳細(行をクリックすると全表示する)</th>
+                @can('admin-role')
+                <th style="width: 10%">編集</th>
+                @endcan
+            </tr>
+        </thead>
+        <tbody>
+          @foreach ($items as $item)
+          <tr class="item-row" data-item-id="{{ $item->id }}">
+              <td>{{ $item->id }}</td>
+              <td>{{ $item->name }}</td>
+              <td>{{ $item->type_name }}</td>
+              <td class="item-detail-cell">
+                  <div class="partial-detail">
+                      {{ \Str::limit($item->detail, 100) }} <!-- 100文字まで表示 -->
+                      @if (strlen($item->detail) > 100)
+                          <span class="text-muted">...</span>
+                      @endif
+                  </div>
+                  <div class="full-detail" style="display: none;">
+                      {{ $item->detail }}
+                  </div>
+              </td>
+              @can('admin-role')
+              <td class="d-flex justify-content-between">
+                  <a href="{{ route('items.edit', $item->id) }}" class="btn btn-primary">編集</a>
+                  <form action="/destroy/{{$item->id}}" method="post">
+                      @csrf
+                      <button class="btn btn-danger" type="submit">削除</button>
+                  </form>
+              </td>
+              @endcan
+          </tr>
+          @endforeach
+          
+          
+          
+        </tbody>
+    </table>
+</div>
+<script>
+  // 各アイテム行にクリックイベントリスナーを追加します
+  document.querySelectorAll('.item-row').forEach(function (row) {
+      row.addEventListener('click', function () {
+          // 詳細情報セル内の要素を取得
+          var partialDetail = row.querySelector('.partial-detail');
+          var fullDetail = row.querySelector('.full-detail');
+
+          // 詳細情報の表示を切り替えます
+          if (partialDetail.style.display === 'none' || partialDetail.style.display === '') {
+              partialDetail.style.display = 'block';
+              fullDetail.style.display = 'none';
+          } else {
+              partialDetail.style.display = 'none';
+              fullDetail.style.display = 'block';
+          }
+      });
+  });
+</script>
+
+
+
 @endsection
